4f74bc1dfa923b88f48b26d3fa7ff74c28fc4fff
Henrik <guzmanem@hotmail.com>
2020-06-20 17:18:22 -0400

    initial api

---
+++ api.py
@@ -0,0 +1,10 @@
+import flask
+from flask import request, jsonify
+
+app = flask.Flask(__name__)
+
+@app.route('/api/v1/', methods=['POST'])
+def home():
+   return jsonify(request.json)
+
+app.run()
