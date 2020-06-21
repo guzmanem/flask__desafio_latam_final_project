cd5a1e18a94c674da3cc1e6b2faaea1483295585
Henrik <guzmanem@hotmail.com>
2020-06-21 00:01:11 -0400

    test jobs v 2

--- app/main.py
+++ app/main.py
@@ -1,8 +1,15 @@
-import flask
+from flask_crontab import Crontab
 from flask import request, jsonify
+import flask
 
 app = flask.Flask(__name__)
+crontab = Crontab(app)
 
 @app.route('/api/v1/', methods=['POST'])
 def home():
    return jsonify(request.json)
+
+@crontab.job(minute="1", hour="0")
+def my_scheduled_job():
+   print('entrooo al job')
+   app.logger.info('entro')
